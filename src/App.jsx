6766691521dcebe2790a8ea8bc603fc60d7f3aae
Aljazi <aljazi.kb@gmail.com>
2026-02-27 16:33:45 +0300

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import AnswerCard from './Component/AnswerCard.jsx';
 import Card from './Component/Card.jsx';
-import Background from './background.jsx';
+import Plasma from './Plasma.jsx';
 
 
 
@@ -10,30 +10,29 @@ function App() {
   return (
     <>
    
-  <div className="grid grid-cols-1 h-screen w-screen overflow-hidden relative justify-center items-center pt-0 mt-0">
-    
-    <div className="md:mt-0 pt-0 absolute inset-0">
-      <Background 
-        colorStops={["#7cff67","#B19EEF","#5227FF"]}
-        blend={0.5}
-        amplitude={1.0}
-        speed={1}/>
-    </div>
-     
-     <div className="absolute inset-0 flex flex-col items-center justify-center space-y-6">
+  <div  style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100dvh',  background: '#000000' }}>
+     <Plasma 
+        color="#f2ff3b"
+        speed={0.6}
+        direction="forward"
+        scale={1.1}
+        opacity={0.4}
+        mouseInteractive={false}
+      />
+  </div>
+
+     <div className="relative z-10 flex flex-col items-center justify-center min-h-screen space-y-8">
         <Card />
         <AnswerCard />
       </div>
 
 
-         <div className="absolute bottom-4 w-full">
-        <p className="text-center text-gray-500 dark:text-gray-400">
-          Create By <a className="text-blue-600 dark:text-sky-400" href="https://aljazikb.github.io/myPortfolio">Aljazy Banaemah</a>
-        </p>
+      <div className="absolute bottom-4 w-full ">
+          <p className="text-center text-gray-500 dark:text-gray-400">
+            Create By <a className="text-blue-600 dark:text-sky-400" href="https://aljazikb.github.io/myPortfolio">Aljazy Banaemah</a>
+          </p>
       </div>
-    
-    
-</div>
+
    </>
 
   )
